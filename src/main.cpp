e80fd917cba083a461ab3ab8e31fff30ad43b678
Zac Pustejovsky <zaqk.putsky@gmail.com>
2020-08-28 07:18:33 -0400

    moar

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 #include <TimedAction.h>
 #include <MemoryFree.h>
 
+#define Rec 0      //Light sensor output
+#define Laser 2    //Laser module 
+
+
 DisplayInterface displayInterface = DisplayInterface();
+bool detection;
 // PiezoBuzzer piezoBuzzer = PiezoBuzzer();
 
 void setup()
@@ -14,6 +19,9 @@ void setup()
     Serial.print("freeMemory()=");
     Serial.println(freeMemory());
     displayInterface.init();
+    pinMode(Laser, OUTPUT);
+    digitalWrite(Laser, HIGH); //Turning on the laser
+    displayInterface.displayMsg("pwnage");
     // piezoBuzzer.init();
 }
 
@@ -22,7 +30,8 @@ void loop()
     Serial.println(F("main loop:"));
     Serial.print("freeMemory()=");
     Serial.println(freeMemory());
-    displayInterface.displayMsg("LOL");
-    delay(1000);
-    // displayLoopAction.check();
+    short Detect = analogRead(Rec);            //Constanly reading the module value
+    if(Detect < 500)              //The Max value is 760, if someone passes it goes below that (every value lower than 700 can do the work)
+        detection = true;          //The detection is triggered
+        displayInterface.displayMsg("WOWOWOW");
 }
